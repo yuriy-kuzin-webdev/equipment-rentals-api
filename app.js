2fda19cb7b6d45f976d56176ed615d9aaabd2f9e
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-08 21:37:08 +0200

    configured connection

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
+const MONGODB = process.env.MONGODB;
 
-app.listen(PORT, console.log(`App started at port : ${PORT}`));
+mongoose
+  .connect(MONGODB)
+  .then(app.listen(PORT, console.log(`App started at port : ${PORT}`)))
+  .catch((error) => console.log(error));
