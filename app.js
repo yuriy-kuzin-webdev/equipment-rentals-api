8c377ab6e43f821894ee374e310d6d8b5a2b4f20
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-08 21:09:00 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`App started at port : ${PORT}`));
