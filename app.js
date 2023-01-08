eed76e15373e4c912b59aa17a2df87d2d13736ee
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-08 21:54:31 +0200

    removed MONGODB const

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
-const MONGODB = process.env.MONGODB;
 
 mongoose
-  .connect(MONGODB)
+  .connect(process.env.MONGODB)
   .then(app.listen(PORT, console.log(`App started at port : ${PORT}`)))
   .catch((error) => console.log(error));
