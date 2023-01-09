bbd50df800453071b9e68470c38bfa3c6aca21c7
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-09 20:11:27 +0200

    added role middleware

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 const authMiddleware = require("../middleware/authMiddleware");
+const roleMiddleware = require("../middleware/roleMiddleware");
 
 router.post("/signin", authController.signin);
 router.post("/signup", authController.signup);
-router.get("/users", authMiddleware, authController.getUsers);
+router.get("/users", roleMiddleware(["ADMIN"]), authController.getUsers);
 
 module.exports = router;
