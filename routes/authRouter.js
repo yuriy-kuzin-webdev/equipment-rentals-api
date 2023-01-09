3ed011ac0e84b2567e62ee161028bb15051d9704
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2023-01-09 19:30:25 +0200

    added middleware to router

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/signin", authController.signin);
 router.post("/signup", authController.signup);
+router.get("/users", authMiddleware, authController);
 
 module.exports = router;
